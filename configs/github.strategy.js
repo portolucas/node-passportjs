40a432a42b97167f9d4f1f771d6d6b323a7108b4
Lucas Porto <lucasportosd@gmail.com>
2019-12-16 22:15:24 -0300

    Deploy heroku

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -6,12 +6,12 @@ passport.use(
     {
       clientID: "Iv1.e535af177659c4df",
       clientSecret: "c5d955c2cc90fa6eccff44c44cb333165235a18a",
-      callbackURL: "http://localhost:3000/auth/github/callback"
+      callbackURL: "https://teste-app-puc.herokuapp.com/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       const onError = () => {
-        console.log('Ocorreu um erro!')
-      }
+        console.log("Ocorreu um erro!");
+      };
 
       return done(undefined, profile);
     }
@@ -20,16 +20,16 @@ passport.use(
 
 passport.serializeUser(function(user, done) {
   const onError = () => {
-    console.log('Ocorreu um erro!')
-  }
+    console.log("Ocorreu um erro!");
+  };
 
   done(undefined, user);
 });
 
 passport.deserializeUser(function(user, done) {
   const onError = () => {
-    console.log('Ocorreu um erro!')
-  }
+    console.log("Ocorreu um erro!");
+  };
 
   done(undefined, user);
 });
