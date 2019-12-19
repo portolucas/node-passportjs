a7051c9aa614a42fa17c3250fe404af72e6fddc0
Nelson Carvalho <nelsoncarvalhosf@gmail.com>
2019-12-18 21:56:17 -0300

    add style

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var passport = require('passport');
+var passport = require("passport");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "DropaTudo" });
 });
 
-router.get('/auth/github',
-passport.authenticate('github'));
-router.get('/auth/github/callback',
-passport.authenticate('github', { failureRedirect: '/login' }),
-function(req, res) {
-// Successful authentication, redirect home.
-res.redirect('/admin');
-});
+router.get("/auth/github", passport.authenticate("github"));
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/admin");
+  }
+);
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-  // req.user is available for use here
-  return next(); }
-  // denied. redirect to login
-  res.redirect('/')
+    // req.user is available for use here
+    return next();
   }
+  // denied. redirect to login
+  res.redirect("/");
+}
 
-  router.get('/admin', ensureAuthenticated, function(req, res) {
-    res.render('admin', { user: req.session.passport.user })
-    });
+router.get("/admin", ensureAuthenticated, function(req, res) {
+  res.render("admin", { user: req.session.passport.user, title: "DropaTudo" });
+});
 
 module.exports = router;
